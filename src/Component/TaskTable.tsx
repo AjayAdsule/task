067c4253db321374a70a2a2c34852afd8d52979d
Ajay A Adsule <ajayadsule5@gmail.com>
2025-04-03 22:43:05 +0530

    add shortcircut condition

--- src/Component/TaskTable.tsx
+++ src/Component/TaskTable.tsx
@@ -46,41 +46,47 @@ const TaskTable = () => {
           onChange={onChange}
         />
       </div>
-      <table className="w-full bg-white border border-gray-200 mt-4">
-        <thead>
-          <tr className="bg-gray-100 border-b">
-            <th className="py-3 px-4  font-semibold ">Title</th>
-            <th className="py-3 px-4 font-semibold">Description</th>
-            <th className="py-3 px-4  font-semibold">Status</th>
-            <th className="py-3 px-4  font-semibold">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {tasks?.map((task) => (
-            <tr key={task.id} className="border-b">
-              <td className="py-3 px-4">{task.title}</td>
-              <td className="py-3 px-4">{task.description}</td>
-              <td className="py-3 px-4 capitalize">{task.status}</td>
-              <td className="py-3 px-4 flex gap-2 justify-center">
-                <Button
-                  variant={"default"}
-                  className="text-xs rounded-full  w-7 h-7"
-                  onClick={() => onEdit(task)}
-                >
-                  <Pencil className="h-4 w-4" />
-                </Button>
-                <Button
-                  variant={"destructive"}
-                  className="text-xs rounded-full  w-7 h-7"
-                  onClick={() => deleteTask(task.id as number)}
-                >
-                  <Trash2 className="h-4 w-4" />
-                </Button>
-              </td>
+      {tasks ? (
+        <table className="w-full bg-white border border-gray-200 mt-4">
+          <thead>
+            <tr className="bg-gray-100 border-b">
+              <th className="py-3 px-4  font-semibold ">Title</th>
+              <th className="py-3 px-4 font-semibold">Description</th>
+              <th className="py-3 px-4  font-semibold">Status</th>
+              <th className="py-3 px-4  font-semibold">Actions</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {tasks?.map((task) => (
+              <tr key={task.id} className="border-b">
+                <td className="py-3 px-4">{task.title}</td>
+                <td className="py-3 px-4">{task.description}</td>
+                <td className="py-3 px-4 capitalize">{task.status}</td>
+                <td className="py-3 px-4 flex gap-2 justify-center">
+                  <Button
+                    variant={"default"}
+                    className="text-xs rounded-full  w-7 h-7"
+                    onClick={() => onEdit(task)}
+                  >
+                    <Pencil className="h-4 w-4" />
+                  </Button>
+                  <Button
+                    variant={"destructive"}
+                    className="text-xs rounded-full  w-7 h-7"
+                    onClick={() => deleteTask(task.id as number)}
+                  >
+                    <Trash2 className="h-4 w-4" />
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <div className="mt-4">
+          <p className="text-lg font-semibold">No task found</p>
+        </div>
+      )}
     </div>
   );
 };
