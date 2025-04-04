ec144730677e542ba3cf086789eb7ea5436da3d0
Ajay A Adsule <ajayadsule5@gmail.com>
2025-04-04 10:46:37 +0530

    add condition for search bar

--- src/Component/TaskTable.tsx
+++ src/Component/TaskTable.tsx
@@ -38,50 +38,52 @@ const TaskTable = () => {
   const debouncedSearch = useDebounce(handleSearch, 500);
   return (
     <div className="overflow-x-auto mt-6">
-      <div className="flex justify-end">
-        <Input
-          className="w-[200px]"
-          placeholder="search task"
-          value={searchParam}
-          onChange={onChange}
-        />
-      </div>
       {tasks.length > 0 ? (
-        <table className="w-full bg-white border border-gray-200 mt-4">
-          <thead>
-            <tr className="bg-gray-100 border-b">
-              <th className="py-3 px-4  font-semibold ">Title</th>
-              <th className="py-3 px-4 font-semibold">Description</th>
-              <th className="py-3 px-4  font-semibold">Status</th>
-              <th className="py-3 px-4  font-semibold">Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {tasks?.map((task) => (
-              <tr key={task.id} className="border-b">
-                <td className="py-3 px-4">{task.title}</td>
-                <td className="py-3 px-4">{task.description}</td>
-                <td className="py-3 px-4 capitalize">{task.status}</td>
-                <td className="py-3 px-4 flex gap-2 justify-center">
-                  <Button
-                    variant={"default"}
-                    className="text-xs rounded-full  w-7 h-7"
-                    onClick={() => onEdit(task)}
-                  >
-                    <Pencil className="h-4 w-4" />
-                  </Button>
-                  <Button
-                    variant={"destructive"}
-                    className="text-xs rounded-full  w-7 h-7"
-                    onClick={() => deleteTask(task.id as number)}
-                  >
-                    <Trash2 className="h-4 w-4" />
-                  </Button>
-                </td>
+        <>
+          <div className="flex justify-end">
+            <Input
+              className="w-[200px]"
+              placeholder="search task"
+              value={searchParam}
+              onChange={onChange}
+            />
+          </div>
+          <table className="w-full bg-white border border-gray-200 mt-4">
+            <thead>
+              <tr className="bg-gray-100 border-b">
+                <th className="py-3 px-4  font-semibold ">Title</th>
+                <th className="py-3 px-4 font-semibold">Description</th>
+                <th className="py-3 px-4  font-semibold">Status</th>
+                <th className="py-3 px-4  font-semibold">Actions</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {tasks?.map((task) => (
+                <tr key={task.id} className="border-b">
+                  <td className="py-3 px-4">{task.title}</td>
+                  <td className="py-3 px-4">{task.description}</td>
+                  <td className="py-3 px-4 capitalize">{task.status}</td>
+                  <td className="py-3 px-4 flex gap-2 justify-center">
+                    <Button
+                      variant={"default"}
+                      className="text-xs rounded-full  w-7 h-7"
+                      onClick={() => onEdit(task)}
+                    >
+                      <Pencil className="h-4 w-4" />
+                    </Button>
+                    <Button
+                      variant={"destructive"}
+                      className="text-xs rounded-full  w-7 h-7"
+                      onClick={() => deleteTask(task.id as number)}
+                    >
+                      <Trash2 className="h-4 w-4" />
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </>
       ) : (
         <div className="mt-4">
           <p className="text-lg font-semibold">No task found</p>
